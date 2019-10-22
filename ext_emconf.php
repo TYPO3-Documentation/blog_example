0f0def390540bc9873c552fdb99801f9f7600685
Susanne Moog <look@susi.dev>
2019-10-22 19:59:57 +0200

    [TASK] Adjust description and credits

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'A Blog Example for the Extbase Framework',
-    'description' => 'An example extension demonstrating the features of the Extbase Framework. It is the back-ported and tweaked Blog Example package of TYPO3.Flow. Have fun playing with it!',
+    'description' => 'An example extension demonstrating the features of the Extbase Framework. Have fun playing with it!',
     'category' => 'example',
     'author' => 'TYPO3 core team',
     'author_company' => '',
