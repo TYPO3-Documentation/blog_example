8bc585f492f90bf9b72f9a8288d876595460f4f5
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2020-12-12 15:55:40 +0100

    [TASK] Add missing dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '10.4.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0',
+            'typo3' => '10.4.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
