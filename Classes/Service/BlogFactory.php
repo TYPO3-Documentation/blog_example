4ac4d5b07fb5ef2b19951c2a4679e0b3bcd034ca
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2021-02-20 11:58:12 +0100

    [TASK] Remove usage of ObjectManager->get() (#5)

--- Classes/Service/BlogFactory.php
+++ Classes/Service/BlogFactory.php
@@ -11,7 +11,6 @@ use FriendsOfTYPO3\BlogExample\Domain\Model\Person;
 use FriendsOfTYPO3\BlogExample\Domain\Model\Post;
 use FriendsOfTYPO3\BlogExample\Domain\Model\Tag;
 use TYPO3\CMS\Core\SingletonInterface;
-use TYPO3\CMS\Extbase\Object\ObjectManagerInterface;
 
 /*
  * This file is part of the TYPO3 CMS project.
@@ -31,23 +30,6 @@ use TYPO3\CMS\Extbase\Object\ObjectManagerInterface;
  */
 class BlogFactory implements SingletonInterface
 {
-
-    /**
-     * @var \TYPO3\CMS\Extbase\Object\ObjectManager
-     */
-    protected $objectManager;
-
-    /**
-     * Injects the object manager
-     *
-     * @param \TYPO3\CMS\Extbase\Object\ObjectManagerInterface $objectManager
-     * @return void
-     */
-    public function injectObjectManager(ObjectManagerInterface $objectManager)
-    {
-        $this->objectManager = $objectManager;
-    }
-
     /**
      * Returns a sample blog populated with generic data
      * It is also an example how to handle objects and repositories in general
@@ -58,15 +40,15 @@ class BlogFactory implements SingletonInterface
     public function createBlog($blogNumber = 1)
     {
         // initialize blog
-        $blog = $this->objectManager->get(Blog::class);
+        $blog = new Blog();
         $blog->setTitle('Blog #' . $blogNumber);
         $blog->setDescription('A blog about TYPO3 extension development.');
 
         // create author
-        $author = $this->objectManager->get(Person::class, 'Stephen', 'Smith', 'foo.bar@example.com');
+        $author = new Person('Stephen', 'Smith', 'foo.bar@example.com');
 
         // create administrator
-        $administrator = $this->objectManager->get(Administrator::class);
+        $administrator = new Administrator();
         $administrator->setName('John Doe');
         $administrator->setEmail('john.doe@example.com');
         $blog->setAdministrator($administrator);
@@ -75,20 +57,20 @@ class BlogFactory implements SingletonInterface
         for ($postNumber = 1; $postNumber < 6; $postNumber++) {
 
             // create post
-            $post = $this->objectManager->get(Post::class);
+            $post = new Post();
             $post->setTitle('The ' . $postNumber . '. post of blog #' . $blogNumber);
             $post->setAuthor($author);
             $post->setContent('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.');
 
             // create comments
-            $comment = $this->objectManager->get(Comment::class);
+            $comment = new Comment();
             $comment->setDate(new DateTime());
             $comment->setAuthor('Peter Pan');
             $comment->setEmail('peter.pan@example.com');
             $comment->setContent('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.');
             $post->addComment($comment);
 
-            $comment = $this->objectManager->get(Comment::class);
+            $comment = new Comment();
             $comment->setDate(new DateTime('2009-03-19 23:44'));
             $comment->setAuthor('John Smith');
             $comment->setEmail('john@matrix.org');
@@ -97,15 +79,15 @@ class BlogFactory implements SingletonInterface
 
             // create some random tags
             if (random_int(0, 1) > 0) {
-                $tag = $this->objectManager->get(Tag::class, 'MVC');
+                $tag = new Tag('MVC');
                 $post->addTag($tag);
             }
             if (random_int(0, 1) > 0) {
-                $tag = $this->objectManager->get(Tag::class, 'Domain Driven Design');
+                $tag = new Tag('Domain Driven Design');
                 $post->addTag($tag);
             }
             if (random_int(0, 1) > 0) {
-                $tag = $this->objectManager->get(Tag::class, 'TYPO3');
+                $tag = new Tag('TYPO3');
                 $post->addTag($tag);
             }
             // add the post to the current blog
