30a5cf0f49ed0dbc15214edf6b6c1fb68de80285
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-08-07 08:17:25 +0200

    [TASK] Set compatibility to TYPO3 v13/main only (#50)
    Releases: main

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'A Blog Example for the Extbase Framework',
     'description' => 'This extension contains code examples used in TYPO3 explained to describe the use of Extbase',
-    'version' => '12.0.2',
+    'version' => '13.0.0',
     'category' => 'example',
     'author' => 'TYPO3 Documentation Team and contributors',
     'author_company' => '',
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'clearCacheOnLoad' => 1,
     'constraints' => [
         'depends' => [
-            'typo3' => '12.0.0-12.99.99',
+            'typo3' => '13.0.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
