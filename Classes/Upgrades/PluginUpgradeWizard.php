02ae0590da720e7ecb0bcb948bf36bacd57d3f79
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2024-05-02 13:18:23 +0200

    [TASK] Create upgrade wizard for blogexample_pi1 (#99)
    * [BUGFIX] Prevent type errors if subtitle is not set
    
    releases: main
    
    * [TASK] Create upgrade wizard for blogexample_pi1
    
    Had to remove rector from the composer.json as
    typo3/cms-install depends on nikic/php-parser 5 while rector still depends on 4
    
    releases: main

---
+++ Classes/Upgrades/PluginUpgradeWizard.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace T3docs\BlogExample\Upgrades;
+
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Install\Attribute\UpgradeWizard;
+use TYPO3\CMS\Install\Updates\UpgradeWizardInterface;
+
+#[UpgradeWizard('blogExample_pluginUpgradeWizard')]
+final class PluginUpgradeWizard implements UpgradeWizardInterface
+{
+    private const OLD_LIST_TYPE = 'blogexample_pi1';
+    private const NEW_LIST_TYPE = 'blogexample_bloglist';
+    public function __construct(
+        private readonly ConnectionPool $connectionPool,
+    ) {}
+
+    public function getTitle(): string
+    {
+        return 'Rename outdated plugins';
+    }
+
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function executeUpdate(): bool
+    {
+        $queryBuilder = $this->connectionPool->getQueryBuilderForTable('tt_content');
+        $result = $queryBuilder
+            ->update('tt_content')
+            ->where(
+                $queryBuilder->expr()->eq('list_type', $queryBuilder->createNamedParameter(self::OLD_LIST_TYPE)),
+            )
+            ->set('list_type', self::NEW_LIST_TYPE)
+            ->executeStatement();
+        return $result > 0;
+    }
+
+    public function updateNecessary(): bool
+    {
+        $queryBuilder = $this->connectionPool->getQueryBuilderForTable('tt_content');
+        $count = $queryBuilder
+            ->count('uid')
+            ->from('tt_content')
+            ->where(
+                $queryBuilder->expr()->eq('list_type', $queryBuilder->createNamedParameter(self::OLD_LIST_TYPE)),
+            )
+            ->executeQuery()
+            ->fetchOne();
+        return is_int($count) && $count > 0;
+    }
+
+    public function getPrerequisites(): array
+    {
+        return [];
+    }
+}
