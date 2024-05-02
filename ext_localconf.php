b6c302c733678b8afa55d7eb8bbf621883f98046
lina.wolf <lwolf@w-commerce.de>
2024-05-02 12:20:08 +0200

    [TASK] Consolidate plugins
    Remove pi1 plugin
    resolves https://github.com/TYPO3-Documentation/blog_example/issues/44
    releases: main

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,99 +5,50 @@ defined('TYPO3') or die();
 use T3docs\BlogExample\Controller\BlogController;
 use T3docs\BlogExample\Controller\CommentController;
 use T3docs\BlogExample\Controller\PostController;
-use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 (static function (string $extensionName): void {
-    /**
-     * Configure the Plugin to call the
-     * right combination of Controller and Action according to
-     * the user input (default settings, FlexForm, URL etc.)
-     */
-    if (
-        GeneralUtility::makeInstance(ExtensionConfiguration::class)->get(
-            'blog_example',
-            'registerSinglePlugin',
-        )
-    ) {
-        ExtensionUtility::configurePlugin(
-            $extensionName,
-            'Pi1',
-            // Cache-able Controller-Actions
-            [
-                BlogController::class => 'index,new,create,delete,deleteAll,edit,update,populate',
-                PostController::class => 'index,show,new,create,delete,edit,update',
-                CommentController::class => 'create,delete',
-            ],
-            // Non-Cache-able Controller-Actions
-            [
-                BlogController::class => 'create,delete,deleteAll,update,populate',
-                PostController::class => 'create,delete,update',
-                CommentController::class => 'create,delete',
-            ],
-        );
-    } else {
-        // Blog plugins
-        ExtensionUtility::configurePlugin(
-            $extensionName,
-            'BlogList',
-            // Cache-able Controller-Actions
-            [
-                BlogController::class => 'index',
-            ],
-        );
+    // Blog plugins
+    ExtensionUtility::configurePlugin(
+        $extensionName,
+        'BlogList',
+        // Cache-able Controller-Actions
+        [
+            BlogController::class => 'index',
+            PostController::class => 'index, show',
+            CommentController::class => 'create',
+        ],
+        [
+            CommentController::class => 'create',
+        ],
+    );
 
-        // Post plugins
-        ExtensionUtility::configurePlugin(
-            $extensionName,
-            'PostList',
-            // Cache-able Controller-Actions
-            [
-                PostController::class => 'index',
-            ],
-        );
-        ExtensionUtility::configurePlugin(
-            'BlogExample',
-            'PostSingle',
-            // Cache-able Controller-Actions
-            [
-                PostController::class => 'show',
-                CommentController::class => 'create',
-            ],
-            // Non-Cache-able Controller-Actions
-            [
-                CommentController::class => 'create',
-            ],
-        );
+    // RSS Feed
+    ExtensionUtility::configurePlugin(
+        $extensionName,
+        'PostListRss',
+        // Cache-able Controller-Actions
+        [
+            PostController::class => 'displayRssList',
+        ],
+    );
 
-        // RSS Feed
-        ExtensionUtility::configurePlugin(
-            $extensionName,
-            'PostListRss',
-            // Cache-able Controller-Actions
-            [
-                PostController::class => 'displayRssList',
-            ],
-        );
-
-        // admin plugins
-        ExtensionUtility::configurePlugin(
-            $extensionName,
-            'BlogAdmin',
-            // Cache-able Controller-Actions
-            [
-                BlogController::class => 'new,create,delete,deleteAll,edit,update,populate',
-                PostController::class => 'new,create,delete,edit,update',
-                CommentController::class => 'delete',
-            ],
-            // Non-Cache-able Controller-Actions
-            [
-                BlogController::class => 'create,delete,deleteAll,update,populate',
-                PostController::class => 'create,delete,update',
-                CommentController::class => 'delete',
-            ],
-        );
-    }
+    // admin plugins
+    ExtensionUtility::configurePlugin(
+        $extensionName,
+        'BlogAdmin',
+        // Cache-able Controller-Actions
+        [
+            BlogController::class => 'new,create,delete,deleteAll,edit,update,populate',
+            PostController::class => 'new,create,delete,edit,update',
+            CommentController::class => 'delete',
+        ],
+        // Non-Cache-able Controller-Actions
+        [
+            BlogController::class => 'create,delete,deleteAll,update,populate',
+            PostController::class => 'create,delete,update',
+            CommentController::class => 'delete',
+        ],
+    );
 })('BlogExample');
