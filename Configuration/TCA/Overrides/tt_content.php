1223d17205fa55bbea9919ed06597cc743a666e6
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2024-05-02 12:27:20 +0200

    [TASK] Consolidate plugins (#97)
    Remove pi1 plugin
    resolves https://github.com/TYPO3-Documentation/blog_example/issues/44
    releases: main

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -13,52 +13,24 @@ defined('TYPO3') or die();
  * Registers a Plugin to be listed in the Backend.
  */
 $extensionConfiguration = GeneralUtility::makeInstance(ExtensionConfiguration::class);
-if ($extensionConfiguration->get('blog_example', 'registerSinglePlugin')) {
-    ExtensionUtility::registerPlugin(
-        'blog_example',
-        'Pi1',
-        'A Blog Example', // A title shown in the backend dropdown field
-    );
-    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['blogexample_pi1']
-        = 'select_key';
-    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['blogexample_pi1']
-        = 'pi_flexform,recursive';
-    ExtensionManagementUtility::addPiFlexFormValue(
-        'blogexample_pi1',
-        'FILE:EXT:blog_example/Configuration/FlexForms/PluginSettings.xml',
-    );
-} else {
-    ExtensionUtility::registerPlugin(
-        'blog_example',
-        'BlogList',
-        'List of Blogs (BlogExample)',
-        'blog_example_icon',
-    );
-    ExtensionUtility::registerPlugin(
-        'blog_example',
-        'PostList',
-        'List of Posts (BlogExample)',
-        'blog_example_icon',
-    );
-    ExtensionUtility::registerPlugin(
-        'blog_example',
-        'PostSingle',
-        'Single Post (BlogExample)',
-        'blog_example_icon',
-    );
-    ExtensionUtility::registerPlugin(
-        'blog_example',
-        'BlogAdmin',
-        'Admin Plugin (BlogExample)',
-        'blog_example_icon',
-    );
+ExtensionUtility::registerPlugin(
+    'blog_example',
+    'BlogList',
+    'List of Blogs (BlogExample)',
+    'blog_example_icon',
+);
+ExtensionUtility::registerPlugin(
+    'blog_example',
+    'BlogAdmin',
+    'Admin Plugin (BlogExample)',
+    'blog_example_icon',
+);
 
-    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['blogexample_postlist']
-        = 'select_key';
-    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['blogexample_postlist']
-        = 'pi_flexform,recursive';
-    ExtensionManagementUtility::addPiFlexFormValue(
-        'blogexample_postlist',
-        'FILE:EXT:blog_example/Configuration/FlexForms/PluginSettings.xml',
-    );
-}
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['blogexample_postlist']
+    = 'select_key';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['blogexample_postlist']
+    = 'pi_flexform,recursive';
+ExtensionManagementUtility::addPiFlexFormValue(
+    'blogexample_postlist',
+    'FILE:EXT:blog_example/Configuration/FlexForms/PluginSettings.xml',
+);
