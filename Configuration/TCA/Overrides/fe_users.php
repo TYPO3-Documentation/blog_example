e5e6da667e4028be737f7fab059e5b7020a43ab8
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2024-05-25 17:03:12 +0200

    [TASK] Fix TCA deprecations (#107)

--- Configuration/TCA/Overrides/fe_users.php
+++ Configuration/TCA/Overrides/fe_users.php
@@ -9,7 +9,7 @@ defined('TYPO3') or die();
 if (is_array($GLOBALS['TCA']['fe_users']['columns']['tx_extbase_type'])) {
     $GLOBALS['TCA']['fe_users']['types'][Administrator::class] = $GLOBALS['TCA']['fe_users']['types']['0'];
     $GLOBALS['TCA']['fe_users']['columns']['tx_extbase_type']['config']['items'][] = [
-        'LLL:EXT:blog_example/Resources/Private/Language/locallang_db.xlf:fe_users.tx_extbase_type.administrator',
-        Administrator::class,
+        'label' => 'LLL:EXT:blog_example/Resources/Private/Language/locallang_db.xlf:fe_users.tx_extbase_type.administrator',
+        'value' => Administrator::class,
     ];
 }
