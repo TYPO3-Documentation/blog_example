330ffb7900e44710de7e31734215c1d6acd399f7
Stefan Froemken <froemken@gmail.com>
2024-06-03 23:28:45 +0200

    Remove is null check. If title is null an underscore can never be found.

--- Classes/Domain/Validator/TitleValidator.php
+++ Classes/Domain/Validator/TitleValidator.php
@@ -22,7 +22,7 @@ final class TitleValidator extends AbstractValidator
     protected function isValid(mixed $value): void
     {
         // $value is the title string
-        if (($value === null || is_scalar($value)) && str_starts_with((string)$value, '_')) {
+        if (is_scalar($value) && str_starts_with((string)$value, '_')) {
             $errorString = 'The title may not start with an underscore. ';
             $this->addError($errorString, 1297418976);
         }
