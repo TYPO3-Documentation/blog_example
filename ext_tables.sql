dd6188e9b0f3a88aeab5263a7254ea5e4ea84eeb
lina.wolf <lwolf@w-commerce.de>
2024-06-03 11:13:33 +0200

    [TASK] Clean up ext_tables.sql
    All removed fields are created automatically in the correct SQL types with TYPO3 13.

--- ext_tables.sql
+++ ext_tables.sql
@@ -4,43 +4,21 @@
 CREATE TABLE tx_blogexample_domain_model_blog (
 	title varchar(255) DEFAULT '' NOT NULL,
 	subtitle varchar(255) DEFAULT '',
-	description text NOT NULL,
-	logo tinyblob DEFAULT NULL,
-	administrator int(11) UNSIGNED NOT NULL DEFAULT '0',
-
-	posts varchar(255) DEFAULT '' NOT NULL
 );
 
 #
 # Table structure for table 'tx_blogexample_domain_model_post'
 #
 CREATE TABLE tx_blogexample_domain_model_post (
-	blog int(11) DEFAULT '0' NOT NULL,
-
 	title varchar(255) DEFAULT '' NOT NULL,
-	date int(11) DEFAULT '0' NOT NULL,
-	author int(11) DEFAULT '0' NOT NULL,
-	second_author int(11) DEFAULT '0' NOT NULL,
-	reviewer int(11) DEFAULT '0' NOT NULL,
-	content text NOT NULL,
-	tags int(11) unsigned DEFAULT '0' NOT NULL,
-	comments int(11) unsigned DEFAULT '0' NOT NULL,
-	related_posts int(11) unsigned DEFAULT '0' NOT NULL,
-	additional_name varchar(255) DEFAULT '' NOT NULL,
-	additional_info int(11) DEFAULT '0' NOT NULL,
-	additional_comments varchar(255) DEFAULT '' NOT NULL
 );
 
 #
 # Table structure for table 'tx_blogexample_domain_model_comment'
 #
 CREATE TABLE tx_blogexample_domain_model_comment (
-	post int(11) DEFAULT '0' NOT NULL,
-
-	date datetime,
 	author varchar(255) DEFAULT '' NOT NULL,
 	email varchar(255) DEFAULT '' NOT NULL,
-	content text NOT NULL
 );
 
 #
@@ -50,8 +28,6 @@ CREATE TABLE tx_blogexample_domain_model_person (
 	firstname varchar(255) DEFAULT '' NOT NULL,
 	lastname varchar(255) DEFAULT '' NOT NULL,
 	email varchar(255) DEFAULT '' NOT NULL,
-	tags int(11) unsigned DEFAULT '0' NOT NULL,
-	tags_special int(11) unsigned DEFAULT '0' NOT NULL
 );
 
 #
@@ -59,8 +35,6 @@ CREATE TABLE tx_blogexample_domain_model_person (
 #
 CREATE TABLE tx_blogexample_domain_model_tag (
 	name varchar(255) DEFAULT '' NOT NULL,
-	priority int(11) DEFAULT '0' NOT NULL,
-	posts int(11) unsigned DEFAULT '0' NOT NULL
 );
 
 
@@ -69,8 +43,6 @@ CREATE TABLE tx_blogexample_domain_model_tag (
 #
 CREATE TABLE tx_blogexample_domain_model_info (
 	name varchar(255) DEFAULT '' NOT NULL,
-	bodytext text,
-	post int(11) DEFAULT '0' NOT NULL
 );
 
 #
