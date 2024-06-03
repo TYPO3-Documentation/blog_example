d831287514df7a0cb7811f7183625f725faa3107
Stefan Froemken <froemken@gmail.com>
2024-06-03 23:22:53 +0200

    Add unit test for TitleValidator

--- Classes/Domain/Validator/TitleValidator.php
+++ Classes/Domain/Validator/TitleValidator.php
@@ -22,7 +22,7 @@ final class TitleValidator extends AbstractValidator
     protected function isValid(mixed $value): void
     {
         // $value is the title string
-        if (str_starts_with('_', $value)) {
+        if (($value === null || is_scalar($value)) && str_starts_with((string)$value, '_')) {
             $errorString = 'The title may not start with an underscore. ';
             $this->addError($errorString, 1297418976);
         }
