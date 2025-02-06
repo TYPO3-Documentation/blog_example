9b052f518ea46f05f2d4918a21e7527d22f32d88
Oliver Hader <oliver@typo3.org>
2025-02-06 15:29:58 +0100

    [BUGFIX] Use proper view variable assignment
    Fixes: #157

--- Classes/Controller/PostController.php
+++ Classes/Controller/PostController.php
@@ -103,7 +103,7 @@ class PostController extends \T3docs\BlogExample\Controller\AbstractController
         $pagination = new SimplePagination($paginator);
         $this->view->assignMultiple([
             'paginator' => $paginator,
-            'pagination', $pagination,
+            'pagination' => $pagination,
             'pages' => range(1, $pagination->getLastPageNumber()),
             'blog' => $blog,
             'posts' => $posts,
