5379ad35f599f28cd4eebf3273a3c51279769bc1
lina.wolf <lwolf@w-commerce.de>
2025-11-19 17:23:22 +0100

    [TASK] Autocreate tables for TCA
    Releases: main, 13.4

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,51 +1,4 @@
 #
-# Table structure for table 'tx_blogexample_domain_model_blog'
-#
-CREATE TABLE tx_blogexample_domain_model_blog (
-	title varchar(255) DEFAULT '' NOT NULL,
-	subtitle varchar(255) DEFAULT '',
-);
-
-#
-# Table structure for table 'tx_blogexample_domain_model_post'
-#
-CREATE TABLE tx_blogexample_domain_model_post (
-	title varchar(255) DEFAULT '' NOT NULL,
-);
-
-#
-# Table structure for table 'tx_blogexample_domain_model_comment'
-#
-CREATE TABLE tx_blogexample_domain_model_comment (
-	author varchar(255) DEFAULT '' NOT NULL,
-	email varchar(255) DEFAULT '' NOT NULL,
-);
-
-#
-# Table structure for table 'tx_blogexample_domain_model_person'
-#
-CREATE TABLE tx_blogexample_domain_model_person (
-	firstname varchar(255) DEFAULT '' NOT NULL,
-	lastname varchar(255) DEFAULT '' NOT NULL,
-	email varchar(255) DEFAULT '' NOT NULL,
-);
-
-#
-# Table structure for table 'tx_blogexample_domain_model_tag'
-#
-CREATE TABLE tx_blogexample_domain_model_tag (
-	name varchar(255) DEFAULT '' NOT NULL,
-);
-
-
-#
-# Table structure for table 'tx_blogexample_domain_model_info'
-#
-CREATE TABLE tx_blogexample_domain_model_info (
-	name varchar(255) DEFAULT '' NOT NULL,
-);
-
-#
 # Table structure for table 'tx_blogexample_domain_model_tag_mm'
 # @TODO fix tx_blogexample_domain_model_person to create and recognize this mm-table
 #       with the field `fieldname` automatically, remove this entry
@@ -54,4 +7,3 @@ CREATE TABLE tx_blogexample_domain_model_info (
 CREATE TABLE `tx_blogexample_domain_model_tag_mm` (
 	`fieldname` varchar(63) DEFAULT '' NOT NULL
 );
-
